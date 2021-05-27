43c495b8bd6c828a67dd4bd8d8420692df148c42
AS <ai.sugita1229@gmail.com>
2021-05-27 10:50:48 +0900

    firebase戻し

--- blog-vue/src/main.js
+++ blog-vue/src/main.js
@@ -6,7 +6,6 @@ import router from './router'
 import GlobalValiables from './mixins/globalValiables'
 import axios from 'axios' 
 import VueAxios from 'vue-axios'
-const IMG_BASE_URL = '../assets/image/'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -17,7 +16,7 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.mixin(GlobalValiables)
 Vue.use(VueAxios, axios) 
-console.log('lint避け' + marked + IMG_BASE_URL)
+console.log(marked)
 new Vue({
   router,
   render: h => h(App),
--- blog-vue/src/views/article/index.vue
+++ blog-vue/src/views/article/index.vue
@@ -12,7 +12,7 @@
           </div>
           <div class="article_update">
             <router-link :to="{path:'article/edit',query:{data:true}}"><span><font-awesome-icon icon="edit" /></span></router-link>
-            <span><font-awesome-icon icon="trash-alt" /></span>
+            <span @click="deleteArticle"><font-awesome-icon icon="trash-alt" /></span>
           </div>
         </div>
       </main>
@@ -33,14 +33,20 @@ export default {
     }
   },
   beforeCreate(){
-  },
-  mounted(){
     if(this.$route.params.data){
       localStorage.setItem('article', JSON.stringify(this.$route.params.data))
       this.data = this.$route.params.data
     }else{
       this.data = JSON.parse(localStorage.getItem('article'))
     }
+  },
+  mounted(){
+    // if(this.$route.params.data){
+    //   localStorage.setItem('article', JSON.stringify(this.$route.params.data))
+    //   this.data = this.$route.params.data
+    // }else{
+    //   this.data = JSON.parse(localStorage.getItem('article'))
+    // }
     
     this.getText()
     this.getTags()
@@ -50,7 +56,16 @@ export default {
       this.$nextTick(() => this.$refs.article_text.innerHTML = marked(this.data.text))
     },
     getTags(){
+      if(this.data.tag === String){
       this.data.tag = this.data.tag.split(',');
+      }
+    },
+    deleteArticle(){
+      const result = window.confirm('本当に削除しますか？')
+      if(result){
+        this.$store.commit('deleteArticle',this.data.id)
+        this.$router.push({path:'/', name:'Home'})
+      }
     }
   }
 }
